69d8a111f3604620205a7d8c92997d4f2322d0ee
Cortezkevin <A19105415@idat.edu.pe>
2025-01-09 23:09:17 -0500

    [fix] - Resolviendo problema de condicional en el controlador de excepciones

--- src/main/java/com/kevin/todo_app/exception/ExceptionAttributes.java
+++ src/main/java/com/kevin/todo_app/exception/ExceptionAttributes.java
@@ -19,54 +19,53 @@ public class ExceptionAttributes extends DefaultErrorAttributes {
     public Map<String, Object> getErrorAttributes(ServerRequest request, ErrorAttributeOptions options) {
         Map<String, Object> errorAttributes = new HashMap<>();
         Throwable throwable = super.getError(request);
-        if(throwable instanceof ResourceNotFoundException resourceNotFoundException){
-            errorAttributes.put("status", HttpStatus.NOT_FOUND);
-            errorAttributes.put("message", resourceNotFoundException.getMessage());
-        }
-        if(throwable instanceof AlreadyExistsResourceWithFieldException alreadyExistsResourceWithFieldException){
-            errorAttributes.put("status", HttpStatus.BAD_REQUEST);
-            errorAttributes.put("message", alreadyExistsResourceWithFieldException.getMessage());
-        }
-        if(throwable instanceof PasswordsNotMatchException passwordsNotMatchException){
-            errorAttributes.put("status", HttpStatus.BAD_REQUEST);
-            errorAttributes.put("message", passwordsNotMatchException.getMessage());
-        }
-        if(throwable instanceof InvalidCredentialsException invalidCredentialsException){
-            errorAttributes.put("status", HttpStatus.BAD_REQUEST);
-            errorAttributes.put("message", invalidCredentialsException.getMessage());
-        }
-        if(throwable instanceof NotInTheTrashException notInTheTrashException){
-            errorAttributes.put("status", HttpStatus.BAD_REQUEST);
-            errorAttributes.put("message", notInTheTrashException.getMessage());
-        }
-        if ( throwable instanceof ConstraintsFieldException constraintsFieldException ){
-            errorAttributes.put("fields", constraintsFieldException.getFieldsErrors());
-            errorAttributes.put("status", HttpStatus.BAD_REQUEST);
-            errorAttributes.put("message", constraintsFieldException.getMessage());
-        }
-        if( throwable instanceof MalformedJwtException malformedJwtException){
-            errorAttributes.put("message", malformedJwtException.getMessage());
-            errorAttributes.put("status", HttpStatus.BAD_REQUEST);
-        }
-        if( throwable instanceof SignatureException signatureException){
-            errorAttributes.put("message", "The token is invalid");
-            errorAttributes.put("detailError", signatureException.getMessage());
-            errorAttributes.put("status", HttpStatus.BAD_REQUEST);
-        }
-        if( throwable instanceof MalformedJwtException malformedJwtException){
-            errorAttributes.put("message", "The token is malformed");
-            errorAttributes.put("detailError", malformedJwtException.getMessage());
-            errorAttributes.put("status", HttpStatus.BAD_REQUEST);
-        }
-        if( throwable instanceof ExpiredJwtException expiredJwtException){
-            errorAttributes.put("message", "The session has expired");
-            errorAttributes.put("detailError", expiredJwtException.getMessage());
-            errorAttributes.put("status", HttpStatus.BAD_REQUEST);
-        }else {
-            errorAttributes.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
-            errorAttributes.put("message", "An unexpected error occurred.");
-            errorAttributes.put("detailError", throwable.getMessage());
-            errorAttributes.put("errorType", "UnknownException");
+        switch (throwable) {
+            case ResourceNotFoundException resourceNotFoundException -> {
+                errorAttributes.put("status", HttpStatus.NOT_FOUND);
+                errorAttributes.put("message", resourceNotFoundException.getMessage());
+            }
+            case AlreadyExistsResourceWithFieldException alreadyExistsResourceWithFieldException -> {
+                errorAttributes.put("status", HttpStatus.BAD_REQUEST);
+                errorAttributes.put("message", alreadyExistsResourceWithFieldException.getMessage());
+            }
+            case PasswordsNotMatchException passwordsNotMatchException -> {
+                errorAttributes.put("status", HttpStatus.BAD_REQUEST);
+                errorAttributes.put("message", passwordsNotMatchException.getMessage());
+            }
+            case InvalidCredentialsException invalidCredentialsException -> {
+                errorAttributes.put("status", HttpStatus.BAD_REQUEST);
+                errorAttributes.put("message", invalidCredentialsException.getMessage());
+            }
+            case NotInTheTrashException notInTheTrashException -> {
+                errorAttributes.put("status", HttpStatus.BAD_REQUEST);
+                errorAttributes.put("message", notInTheTrashException.getMessage());
+            }
+            case ConstraintsFieldException constraintsFieldException -> {
+                errorAttributes.put("fields", constraintsFieldException.getFieldsErrors());
+                errorAttributes.put("status", HttpStatus.BAD_REQUEST);
+                errorAttributes.put("message", constraintsFieldException.getMessage());
+            }
+            case MalformedJwtException malformedJwtException -> {
+                errorAttributes.put("message", "The token is malformed");
+                errorAttributes.put("detailError", malformedJwtException.getMessage());
+                errorAttributes.put("status", HttpStatus.BAD_REQUEST);
+            }
+            case SignatureException signatureException -> {
+                errorAttributes.put("message", "The token is invalid");
+                errorAttributes.put("detailError", signatureException.getMessage());
+                errorAttributes.put("status", HttpStatus.BAD_REQUEST);
+            }
+            case ExpiredJwtException expiredJwtException -> {
+                errorAttributes.put("message", "The session has expired");
+                errorAttributes.put("detailError", expiredJwtException.getMessage());
+                errorAttributes.put("status", HttpStatus.BAD_REQUEST);
+            }
+            case null, default -> {
+                errorAttributes.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+                errorAttributes.put("message", "An unexpected error occurred.");
+                errorAttributes.put("detailError", throwable.getMessage());
+                errorAttributes.put("errorType", "UnknownException");
+            }
         }
         return errorAttributes;
     }
--- src/main/java/com/kevin/todo_app/handler/AuthHandler.java
+++ src/main/java/com/kevin/todo_app/handler/AuthHandler.java
@@ -3,9 +3,11 @@ package com.kevin.todo_app.handler;
 import com.kevin.todo_app.dto.user.CreateUserDTO;
 import com.kevin.todo_app.dto.user.JwtDTO;
 import com.kevin.todo_app.dto.user.LoginUserDTO;
+import com.kevin.todo_app.exception.ConstraintsFieldException;
 import com.kevin.todo_app.helpers.ObjectValidator;
 import com.kevin.todo_app.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -19,7 +21,7 @@ public class AuthHandler {
     private final AuthService authService;
     private final ObjectValidator validator;
 
-    public Mono<ServerResponse> create(ServerRequest req){
+    public Mono<ServerResponse> create(ServerRequest req) {
         Mono<CreateUserDTO> createUserDTOMono = req.bodyToMono(CreateUserDTO.class).doOnNext(validator::validate);
         return  createUserDTOMono.flatMap( createUserDTO -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body( authService.create(createUserDTO), JwtDTO.class ) );
     }
