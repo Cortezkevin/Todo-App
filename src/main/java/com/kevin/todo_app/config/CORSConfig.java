36c365aef3d1aa539f371a1b9e902d0c8f017202
Cortezkevin <A19105415@idat.edu.pe>
2025-01-17 23:40:18 -0500

    [feat] - Agregando configuración de CORS

---
+++ src/main/java/com/kevin/todo_app/config/CORSConfig.java
@@ -0,0 +1,20 @@
+package com.kevin.todo_app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class CORSConfig implements WebFluxConfigurer {
+
+   @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials( false );
+    }
+}
