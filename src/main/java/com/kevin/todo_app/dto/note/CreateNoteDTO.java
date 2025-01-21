2bf8de24e5a094cec5ab45c28a31bf84f293d83a
Cortezkevin <A19105415@idat.edu.pe>
2025-01-20 23:44:30 -0500

    [refactor] - Organizando codigo

--- src/main/java/com/kevin/todo_app/dto/note/CreateNoteDTO.java
+++ src/main/java/com/kevin/todo_app/dto/note/CreateNoteDTO.java
@@ -1,7 +1,6 @@
 package com.kevin.todo_app.dto.note;
 
 import com.kevin.todo_app.documents.note.Element;
-import com.kevin.todo_app.enums.Color;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
--- src/main/java/com/kevin/todo_app/dto/note/DetailedNoteDTO.java
+++ src/main/java/com/kevin/todo_app/dto/note/DetailedNoteDTO.java
@@ -2,7 +2,6 @@ package com.kevin.todo_app.dto.note;
 
 import com.kevin.todo_app.documents.note.Element;
 import com.kevin.todo_app.documents.note.Note;
-import com.kevin.todo_app.enums.Color;
 
 import java.time.LocalDateTime;
 import java.util.List;
